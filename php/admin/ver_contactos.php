116f863846c722314ea6afe91f6e1c32d90b0c78
Nicolas Mastromarino <nicolasmastrom@gmail.com>
2023-04-24 14:23:25 -0300

    camio de realm

--- php/admin/ver_contactos.php
+++ php/admin/ver_contactos.php
@@ -10,7 +10,7 @@ $pass = $_SERVER['PHP_AUTH_PW'];
 $validated = (in_array($user, $valid_users)) && ($pass == $valid_passwords[$user]);
 
 if (!$validated) {
-    header('WWW-Authenticate: Basic realm="My Realm"');
+    header('WWW-Authenticate: Basic realm="Authentication required"');
     header('HTTP/1.0 401 Unauthorized');
     echo "No estás autorizado para ver esta página.";
     exit;
