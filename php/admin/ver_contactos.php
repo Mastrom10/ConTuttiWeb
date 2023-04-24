fb3eceb56552275394e84c022e1b117fa3be3fdc
Nicolas Mastromarino <nicolasmastrom@gmail.com>
2023-04-24 14:25:34 -0300

    sin auth

--- php/admin/ver_contactos.php
+++ php/admin/ver_contactos.php
@@ -1,20 +1,4 @@
 <?php
-$username = 'admin';
-$password = 'cocacola';
-$valid_passwords = array($username => $password);
-$valid_users = array_keys($valid_passwords);
-
-$user = $_SERVER['PHP_AUTH_USER'];
-$pass = $_SERVER['PHP_AUTH_PW'];
-
-$validated = (in_array($user, $valid_users)) && ($pass == $valid_passwords[$user]);
-
-if (!$validated) {
-    header('WWW-Authenticate: Basic realm="Authentication required"');
-    header('HTTP/1.0 401 Unauthorized');
-    echo "No estás autorizado para ver esta página.";
-    exit;
-}
 
 // Conexión a la base de datos usando database.php
 require_once '../database.php';
