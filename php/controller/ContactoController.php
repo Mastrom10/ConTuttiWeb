9ed225f950b9b329121678499601f1dc187ae262
Nicolas Mastromarino <nicolasmastrom@gmail.com>
2023-07-05 12:57:39 -0300

    Ahora llama al webhook

--- php/controller/ContactoController.php
+++ php/controller/ContactoController.php
@@ -39,6 +39,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     $contactoController->registrarContacto($contacto);
 
+
+    // Enviar datos al webhook
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, 'https://hook.us1.make.com/5i3f1rv1f2a44iqch43wqcbbha3tvz3p');
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));  // Establece el encabezado de contenido a "application/json"
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+
+    curl_close($ch);
+
+
     header('Content-Type: application/json');
     echo json_encode(['message' => 'Contacto registrado con exito']);
 }
